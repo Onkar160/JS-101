06e8e5c66be03995b26a696ada4198e4e07c2e7b
onkar <ophulwalkar77@gmail.com>
2025-02-27 11:43:19 +0530

    Add: Session1

--- sess1/sess1.js
+++ sess1/sess1.js
@@ -1,7 +1,39 @@
-console.log("Onkar Chandrakant Phulwalkar");
 
-var greeter;
-greeter = "hey hi";
-console.log(greeter);
-greeter = "Hey";
-console.log(greeter);
+
+// // Printing first msg on console
+// console.log("Onkar Chandrakant Phulwalkar");
+
+
+// // Activity1 - reassignment with var
+// var greeter = "Hey  hi";
+// console.log(greeter);
+// greeter = "Hey";
+// console.log(greeter);
+
+
+
+
+// //Re-declaration or re-initialization is allowed with var keyword
+// var a = 10;  
+// console.log(a);
+// var a;
+// a = 30;
+// console.log(a);
+
+
+
+// Note - Variables are labels(pointer) to the values where they stored in the memory
+
+
+
+// variable names cannot be started with numbers but they can be started underscore, letter or dollar sign
+// and after 1st 
+const $5 = 1;
+console.log($5);
+
+
+/* Interview Questions :-
+    1. Why do we need javascript?
+    2. What are variables? 
+    3. 
+*/
